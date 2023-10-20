7a31b2b3755a952c443d441f05d9ec62ce2a4bb9
sulfurandcu <sulfurandcu@gmail.com>
2023-10-20 18:43:44 +0800

    更新博客配置
    This reverts commit e7e9287fd49f0eb508d991368868469c75cc1388.

--- source/_drafts/opencc繁体转简体转繁体.bat
+++
@@ -1,2 +0,0 @@
-for /r %%i in (*.md) do ( D:\develop.r.1\opencc\bin\opencc.exe -c D:\develop.r.1\opencc\bin\t2s.json -i "%%i" -o "%%i" )
-for /r %%i in (*.md) do ( D:\develop.r.1\opencc\bin\opencc.exe -c D:\develop.r.1\opencc\bin\s2t.json -i "%%i" -o "%%i" )
